b2f4a51d2ea38969fd38c8d1e2da8910a75da3a0
kobe shaw <110267313+Kobesterzz@users.noreply.github.com>
2023-12-20 15:26:16 -0600

    Add files via upload

--- components/SignupForm.jsx
+++ components/SignupForm.jsx
@@ -1,7 +1,8 @@
 "use client";
 import React, { useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
 
-const SignupForm = () => {
+const signupForm = ({ onClose }) => {
   useEffect(() => {
     // Create a script element
     const script = document.createElement('script');
@@ -19,12 +20,58 @@ const SignupForm = () => {
   }, []); 
 
   const signUp = {
-    background: 'white'
+    background : 'white'
   }
 
+  const zoomIn = keyframes`
+  from {
+    transform: scale(0.8);
+    opacity: 0;
+  }
+  to {
+    transform: scale(1);
+    opacity: 1;
+  }
+`;
+
+  const ModalOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1000;
+`;
+
+const ModalContent = styled.div`
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  animation: ${zoomIn} 01s ease-in-out;
+`;
+
+const CloseButton = styled.span`
+  position: absolute;
+  top: 70px;
+  right: 70px;
+  font-size: 50px;
+  cursor: pointer;
+`;
+
   return (
-    <div id='signUp' className="elfsight-app-a26e6c29-beb2-493b-bd03-24cde9c8bfdf" style={signUp} data-elfsight-app-lazy></div>
+    <ModalOverlay>
+      <ModalContent>
+        <CloseButton onClick={onClose}>&times;</CloseButton>
+        {/* Your signup form content goes here */}
+        <div id='signUp' class="elfsight-app-a26e6c29-beb2-493b-bd03-24cde9c8bfdf" style = {signUp} data-elfsight-app-lazy></div>
+      </ModalContent>
+    </ModalOverlay>
   );
 };
 
-export default SignupForm;
+export default signupForm;
+
