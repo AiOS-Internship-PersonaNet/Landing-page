71ee63314c2882d1851d1020aae3a30f63efa333
yodanyc <151226217+yodanyc@users.noreply.github.com>
2023-12-26 14:40:12 -0500

    Update FeaturesSection.jsx

--- components/FeaturesSection.jsx
+++ components/FeaturesSection.jsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
@@ -62,145 +61,278 @@ const FeaturesSection = () => {
       <h2 className="text-3xl lg:text-5xl font-bold mt-0 mb-4 md:mt-0 md:mb-8 absolute top-0 left-1/2 transform -translate-x-1/2">
         Our Features
       </h2>
+      {/* Middle border big screen */}
 
-      {/* Middle border */}
-      <motion.div
-        className="middle-border"
+      {windowWidth > 768 && (
+        <motion.div
+          className="middle-border"
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: middleBorderHovered ? 'translate(-50%, -55%)' : 'translate(-50%, -50%)',
+            width: '30%',
+            height: '70%',
+            border: '2px solid rgba(255, 255, 255, 0.6)',
+            borderRadius: '20px',
+            zIndex: '1',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            transition: 'transform 0.3s ease, background-color 0.3s ease',
+            backgroundColor: middleBorderHovered ? 'rgba(255, 255, 255, 0.3)' : 'rgba(0, 0, 0, 0.5)',
+            fontSize: windowWidth <= 768 ? '0.4em' : '1em',
+          }}
+          onMouseEnter={handleMiddleBorderHover}
+          onMouseLeave={handleMiddleBorderLeave}
+        >
+          <h2 className={`${middleBorderHovered ? 'text-lg' : 'text-xl'} ${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-xl' : ''}`}>
+            Express Your True Self
+          </h2>
+          <Image
+            src="/images/background2.png"
+            alt="Description of the image"
+            width={800}
+            height={600}
+            style={{ maxWidth: '70%', height: 'auto', margin: '10px 0' }}
+          />
+          <p
+            className={`${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-sm' : 'text-base'} opacity-${middleBorderHovered ? '100' : '0'} transition-opacity duration-300 ${middleBorderHovered ? '' : 'hidden'}`}
+            onMouseEnter={handleMiddleBorderHover}
+            onMouseLeave={handleMiddleBorderLeave}
+          >
+            With our innovative persona creation tool, you&apos;re not just building a profile; you&apos;re crafting a digital identity. Answer a few simple questions and let our AI weave a persona that truly represents you.
+          </p>
+        </motion.div>
+      )}
+         {/* Left border big screen*/}
+      {windowWidth > 768 && (
+        <motion.div
+          className="left-border"
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: 'calc(50% - 33%)',
+            transform: leftBorderHovered ? 'translate(-50%, -55%)' : 'translate(-50%, -50%)',
+            width: '30%',
+            height: '70%',
+            border: '2px solid rgba(255, 255, 255, 0.6)',
+            borderRadius: '20px',
+            zIndex: '1',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            transition: 'transform 0.3s ease, background-color 0.3s ease',
+            backgroundColor: leftBorderHovered ? 'rgba(255, 255, 255, 0.3)' : 'rgba(0, 0, 0, 0.5)',
+            fontSize: windowWidth <= 768 ? '0.4em' : '1em',
+          }}
+          onMouseEnter={handleLeftBorderHover}
+          onMouseLeave={handleLeftBorderLeave}
+        >
+          {/* Content for left border */}
+          <h2 className={`${leftBorderHovered ? 'text-base' : 'text-lg'} ${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-xl' : ''}`}>
+            Unleash Your Personality
+          </h2>
+          <Image
+            src="/images/persona.png"
+            alt="Description of the image"
+            width={800}
+            height={600}
+            style={{ maxWidth: '70%', height: 'auto', margin: '10px 0' }}
+          />
+          <p
+            className={`${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-sm' : 'text-base'} opacity-${leftBorderHovered ? '100' : '0'} transition-opacity duration-300 ${leftBorderHovered ? '' : 'hidden'}`}
+          >
+            Create multiple personas that showcase every aspect of your personality. Whether it's engaging in lively debates or sharing a laugh, your personas represent the multifaceted nature of your identity.
+          </p>
+        </motion.div>
+      )}
+
+      {/* Right border big screen */}
+      {windowWidth > 768 && (
+        <motion.div
+          className="right-border"
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: 'calc(50% + 33%)',
+            transform: rightBorderHovered ? 'translate(-50%, -55%)' : 'translate(-50%, -50%)',
+            width: '30%',
+            height: '70%',
+            border: '2px solid rgba(255, 255, 255, 0.6)',
+            borderRadius: '20px',
+            zIndex: '1',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            transition: 'transform 0.3s ease, background-color 0.3s ease',
+            backgroundColor: rightBorderHovered ? 'rgba(255, 255, 255, 0.3)' : 'rgba(0, 0, 0, 0.5)',
+            fontSize: windowWidth <= 768 ? '0.4em' : '1em',
+          }}
+          onMouseEnter={handleRightBorderHover}
+          onMouseLeave={handleRightBorderLeave}
+        >
+          {/* Content for right border */}
+          <h2 className={`${rightBorderHovered ? 'text-base' : 'text-lg'} ${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-xl' : ''}`}>
+          Real-Time Engagement and Notifications
+          </h2>
+          <Image
+            src="/images/chat.png"
+            alt="Description of the image"
+            width={800}
+            height={600}
+            style={{ maxWidth: '100%', height: 'auto', margin: '10px 0' }}
+          />
+          <p
+            className={`${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-sm' : 'text-base'} opacity-${rightBorderHovered ? '100' : '0'} transition-opacity duration-300 ${rightBorderHovered ? '' : 'hidden'}`}
+          >
+            Stay engaged with real-time updates. Whether your persona is getting likes, comments, or matched with like-minded users, you&apos;re always in the loop.
+          </p>
+        </motion.div>
+      )}
+
+      {/* Borders Container for Small Screens */}
+      <div
+        className="borders-container"
         style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: middleBorderHovered ? 'translate(-50%, -55%)' : 'translate(-50%, -50%)',
-          width: '30%',
-          height: '70%',
-          border: '2px solid rgba(255, 255, 255, 0.6)',
-          borderRadius: '20px',
-          zIndex: '1',
-          display: 'flex',
+          display: windowWidth <= 768 ? 'flex' : 'none', // Show only on small screens
           flexDirection: 'column',
-          justifyContent: 'center',
           alignItems: 'center',
-          textAlign: 'center',
-          transition: 'transform 0.3s ease, background-color 0.3s ease',
-          backgroundColor: middleBorderHovered ? 'rgba(255, 255, 255, 0.3)' : 'rgba(0, 0, 0, 0.5)',
-          fontSize: windowWidth <= 768 ? '0.4em' : '1em',
+          justifyContent: 'center',
+          width: '100%',
+          height: '70%',
+          position: 'absolute',
+          top: '55%',
+          transform: 'translateY(-50%)',
         }}
-        onMouseEnter={handleMiddleBorderHover}
-        onMouseLeave={handleMiddleBorderLeave}
       >
-        {/* Express Your True Self Content */}
-        <h2 className={`${middleBorderHovered ? 'text-lg' : 'text-xl'} ${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-xl' : ''}`}>
-  Express Your True Self
-</h2>
-        <Image
+        {/* Middle border small screens */}
+        <motion.div
+          className="middle-border"
+          style={{
+            position: 'relative',
+            width: '80%',
+            border: '2px solid rgba(255, 255, 255, 0.6)',
+            borderRadius: '20px',
+            backgroundColor: middleBorderHovered ? 'rgba(255, 255, 255, 0.3)' : 'rgba(0, 0, 0, 0.5)',
+            fontSize: windowWidth <= 768 ? '0.4em' : '1em',
+            marginBottom: '20px',
+            textAlign: 'center',
+          }}
+          onMouseEnter={handleMiddleBorderHover}
+          onMouseLeave={handleMiddleBorderLeave}
+        >
+          {/* Middle border content */}
+          <h2 className={`${middleBorderHovered ? 'text-lg' : 'text-xl'} ${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-xl' : ''}`}>
+            Express Your True Self
+          </h2>
+          <Image
   src="/images/background2.png"
   alt="Description of the image"
   width={800} 
   height={600} 
-  style={{ maxWidth: '70%', height: 'auto', margin: '10px 0' }}
+  style={{
+    maxWidth: '50%',
+    height: '30',
+    margin: '10px auto',
+    display: 'block', 
+  }}
 />
+          <p
+            className={`${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-sm' : 'text-base'} opacity-${middleBorderHovered ? '100' : '0'} transition-opacity duration-300 ${middleBorderHovered ? '' : 'hidden'}`}
+            onMouseEnter={handleMiddleBorderHover}
+            onMouseLeave={handleMiddleBorderLeave}
+          >
+            With our innovative persona creation tool, you&apos;re not just building a profile; you&apos;re crafting a digital identity. Answer a few simple questions and let our AI weave a persona that truly represents you.
+          </p>
+        </motion.div>
 
-<p
-  className={`${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-sm' : 'text-base'} opacity-${middleBorderHovered ? '100' : '0'} transition-opacity duration-300 ${middleBorderHovered ? '' : 'hidden'}`}
-  onMouseEnter={handleMiddleBorderHover}
-  onMouseLeave={handleMiddleBorderLeave}
->
-  With our innovative persona creation tool, you&apos;re not just building a profile; you&apos;re crafting a digital identity. Answer a few simple questions and let our AI weave a persona that truly represents you.
-</p>
-
-      </motion.div>
-
-      {/* Left border */}
-      <motion.div
-        className="left-border"
-        style={{
-          position: 'absolute',
-          top: '50%',
-          left: 'calc(50% - 33%)',
-          transform: leftBorderHovered ? 'translate(-50%, -55%)' : 'translate(-50%, -50%)',
-          width: '30%',
-          height: '70%',
-          border: '2px solid rgba(255, 255, 255, 0.6)',
-          borderRadius: '20px',
-          zIndex: '1',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          textAlign: 'center',
-          transition: 'transform 0.3s ease, background-color 0.3s ease',
-          backgroundColor: leftBorderHovered ? 'rgba(255, 255, 255, 0.3)' : 'rgba(0, 0, 0, 0.5)',
-          fontSize: windowWidth <= 768 ? '0.4em' : '1em',
-        }}
-        onMouseEnter={handleLeftBorderHover}
-        onMouseLeave={handleLeftBorderLeave}
-      >
-        {/* Unleash Your Personality Content */}
-        <h2 className={`${leftBorderHovered ? 'text-base' : 'text-lg'} ${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-xl' : ''}`}>
-  Unleash Your Personality
-</h2>
-
-        <Image
+        {/* Left border small screens */}
+        <motion.div
+          className="left-border"
+          style={{
+            position: 'relative',
+            width: '80%',
+            border: '2px solid rgba(255, 255, 255, 0.6)',
+            borderRadius: '20px',
+            backgroundColor: leftBorderHovered ? 'rgba(255, 255, 255, 0.3)' : 'rgba(0, 0, 0, 0.5)',
+            fontSize: windowWidth <= 768 ? '0.4em' : '1em',
+            marginBottom: '20px',
+            textAlign: 'center',
+          }}
+          onMouseEnter={handleLeftBorderHover}
+          onMouseLeave={handleLeftBorderLeave}
+        >
+          {/* Left border content */}
+          <h2 className={`${leftBorderHovered ? 'text-base' : 'text-lg'} ${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-xl' : ''}`}>
+            Unleash Your Personality
+          </h2>
+          <Image
   src="/images/persona.png"
   alt="Description of the image"
   width={800} 
   height={600} 
-  style={{ maxWidth: '70%', height: 'auto', margin: '10px 0' }}
+  style={{
+    maxWidth: '50%',
+    height: 'auto',
+    margin: '10px auto',
+    display: 'block', 
+  }}
 />
+          <p
+            className={`${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-sm' : 'text-base'} opacity-${leftBorderHovered ? '100' : '0'} transition-opacity duration-300 ${leftBorderHovered ? '' : 'hidden'}`}
+          >
+            Create multiple personas that showcase every aspect of your personality. Whether it&apos;s engaging in lively debates or sharing a laugh, your personas represent the multifaceted nature of your identity.
+          </p>
+        </motion.div>
 
-<p
-  className={`${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-sm' : 'text-base'} opacity-${leftBorderHovered ? '100' : '0'} transition-opacity duration-300 ${leftBorderHovered ? '' : 'hidden'}`}
->
-  Create multiple personas that showcase every aspect of your personality. Whether it&apos;s engaging in lively debates or sharing a laugh, your personas represent the multifaceted nature of your identity.
-</p>
-      </motion.div>
-
-      {/* Right border */}
-      <motion.div
-        className="right-border"
-        style={{
-          position: 'absolute',
-          top: '50%',
-          left: 'calc(50% + 33%)',
-          transform: rightBorderHovered ? 'translate(-50%, -55%)' : 'translate(-50%, -50%)',
-          width: '30%',
-          height: '70%',
-          border: '2px solid rgba(255, 255, 255, 0.6)',
-          borderRadius: '20px',
-          zIndex: '1',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          textAlign: 'center',
-          transition: 'transform 0.3s ease, background-color 0.3s ease',
-          backgroundColor: rightBorderHovered ? 'rgba(255, 255, 255, 0.3)' : 'rgba(0, 0, 0, 0.5)',
-          fontSize: windowWidth <= 768 ? '0.4em' : '1em',
-        }}
-        onMouseEnter={handleRightBorderHover}
-        onMouseLeave={handleRightBorderLeave}
-      >
-        {/* Real-Time Engagement and Notifications Content */}
-        <h2 className={`${rightBorderHovered ? 'text-base' : 'text-lg'} ${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-xl' : ''}`}>
-  Real-Time Engagement and Notifications
-</h2>
-
-        <Image
+        {/* Right border  small screens*/}
+        <motion.div
+          className="right-border"
+          style={{
+            position: 'relative',
+            width: '80%',
+            border: '2px solid rgba(255, 255, 255, 0.6)',
+            borderRadius: '20px',
+            backgroundColor: rightBorderHovered ? 'rgba(255, 255, 255, 0.3)' : 'rgba(0, 0, 0, 0.5)',
+            fontSize: windowWidth <= 768 ? '0.4em' : '1em',
+            marginBottom: '20px',
+            textAlign: 'center',
+          }}
+          onMouseEnter={handleRightBorderHover}
+          onMouseLeave={handleRightBorderLeave}
+        >
+          {/* Right border content */}
+          <h2 className={`${rightBorderHovered ? 'text-base' : 'text-lg'} ${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-xl' : ''}`}>
+            Real-Time Engagement and Notifications
+          </h2>
+          <Image
   src="/images/chat.png"
   alt="Description of the image"
   width={800} 
   height={600} 
-  style={{ maxWidth: '100%', height: 'auto', margin: '10px 0' }}
+  style={{
+    maxWidth: '100%',
+    height: 'auto',
+    margin: '10px auto', 
+    display: 'block', 
+  }}
 />
-
-<p
-  className={`${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-sm' : 'text-base'} opacity-${rightBorderHovered ? '100' : '0'} transition-opacity duration-300 ${rightBorderHovered ? '' : 'hidden'}`}
-  onMouseEnter={handleRightBorderHover}
-  onMouseLeave={handleRightBorderLeave}
->
-  Stay engaged with real-time updates. Whether your persona is getting likes, comments, or matched with like-minded users, you&apos;re always in the loop.
-</p>
-      </motion.div>
+          <p
+            className={`${typeof window !== 'undefined' && window.innerWidth <= 768 ? 'text-sm' : 'text-base'} opacity-${rightBorderHovered ? '100' : '0'} transition-opacity duration-300 ${rightBorderHovered ? '' : 'hidden'}`}
+            onMouseEnter={handleRightBorderHover}
+            onMouseLeave={handleRightBorderLeave}
+          >
+            Stay engaged with real-time updates. Whether your persona is getting likes, comments, or matched with like-minded users, you&apos;re always in the loop.
+          </p>
+        </motion.div>
+      </div>
 
       {/* Animated Background */}
       <motion.div
