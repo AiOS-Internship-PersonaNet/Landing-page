4eb25f7d16e3d82d76e1c17764f2d5f0e2579dd0
devparpiev <devparpiev@gmail.com>
2024-12-11 15:10:23 -0600

    Changed the color of FreeLife class in header

---
+++ app/HeroSection.jsx
@@ -0,0 +1,77 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import Phone from "@/components/SignCall";
+
+
+
+const HeroSection = () => {
+
+  const [initialPosition, setInitialPosition] = useState('translateY(100%)');
+
+  useEffect(() => {
+    // Set a timeout to delay the animation start
+    const timeout = setTimeout(() => {
+      setInitialPosition('translateY(0)');
+    }, 550);
+
+    // Clear the timeout to avoid memory leaks
+    return () => clearTimeout(timeout);
+  }, []);
+
+
+
+
+  return (
+    <section style={{ overflow: 'hidden',}} className="flex flex-col items-center justify-center h-screen w-full  hover:from-blue-600 hover:to-teal-600 text-white font-medium py-2.5 px-5 rounded-lg  transition relative">
+
+      
+    <div
+        className="absolute top-0 left-0 w-full h-full bg-cover bg-center flex items-end justify-center"
+        style={{
+          background: 'radial-gradient(circle at 0% 0%, #220138, #2b0399, #220138)',
+          
+          backgroundPosition: 'center', // Center the background image
+          backgroundSize: 'fill', 
+          backgroundRepeat: 'no-repeat',
+          bottom:'0',
+          top: 'auto',
+          zIndex: '0'
+        }}
+      ></div>
+
+     
+
+
+    {/* <Design id="tsparticles" style={{ zIndex: '0' }}/> */}
+ 
+    <Phone/>
+
+      
+
+      {/* Mobile Device Frame */}
+      <div className="relative max-w-[90%]">
+        {/* Top and Bottom part of phone */}
+        <div
+          className="bg-gray-400 w-4 h-1 rounded-full mx-auto mb-1 overflow-hidden"
+          style={{ borderRadius: '30px 30px 0 0', display: 'none' }}
+        ></div>
+        {/* Screen */}
+        <div className="relative overflow-hidden">
+          {/* Content with Smaller Image */}
+          <div className="p-6 relative text-center"></div>
+          {/* End of Content with Smaller Image */}
+        </div>
+        {/* Bottom part of phone */}
+        <div
+          className="bg-gray-400 w-4 h-1 rounded-full mx-auto mt-1"
+          style={{ borderRadius: '50%', display: 'none' }}
+        ></div>
+      </div>
+      {/* End of Mobile Device Frame */}
+
+
+    </section>
+  );
+};
+
+export default HeroSection;
--- components/SignCall.jsx
+++ components/SignCall.jsx
@@ -1,42 +1,41 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
 
-
 // Styled components
 const Container = styled.div`
-    position: relative;
-    height: 100vh;
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    width: 100%;
+	position: relative;
+	height: 100vh;
+	color: white;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+	width: 100%;
 `;
 
 const Content = styled.div`
-    position: relative;
-    width: 100%;
+	position: relative;
+	width: 100%;
 `;
 
 const StyledRow = styled.div`
-    display: flex;
-    width: 100%;
-    justify-content: center;
+	display: flex;
+	width: 100%;
+	justify-content: center;
 `;
 
 const SideContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 10px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 10px;
 `;
 
-const MiddleContainer = styled.div`    
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex: 0 0 auto;
+const MiddleContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex: 0 0 auto;
 `;
 
 const SideImage = styled.img`
@@ -49,113 +48,112 @@ const SideImage = styled.img`
 `;
 
 const MiddleImage = styled.img`
-    height: 300px;
-    width: 300px;
-    border-radius: 50%;
+	height: 300px;
+	width: 300px;
+	border-radius: 50%;
 `;
 
 const Header = styled.div`
-    font-size: 2rem;
-    margin-bottom: 10px;
-    font-weight: bold;
+	font-size: 2rem;
+	margin-bottom: 10px;
+	font-weight: bold;
+    .FreeLife{
+    color: #919aff;
+    }
 `;
 
 const SubHeader = styled.div`
-    font-size: 1rem;
-    margin-bottom: 10px;
-    font-weight: bold;
+	font-size: 1rem;
+	margin-bottom: 10px;
+	font-weight: bold;
 `;
 
-
 const TopRightImage = styled.img`
-  position: absolute;
-  top: 0%;
-  right: 0%;
-  width: 100px;
-  height: 100px;
-  border-radius: 50%
+	position: absolute;
+	top: 0%;
+	right: 0%;
+	width: 100px;
+	height: 100px;
+	border-radius: 50%;
 `;
 
 const TopLeftImage = styled.img`
-  position: absolute;
-  top: 0%;
-  left: 0%;
-  
+	position: absolute;
+	top: 0%;
+	left: 0%;
 `;
 
 const ButtonSection = styled.div`
-    justify-content: center;
-    margin-top: 50px;
-    display: flex;
-    gap: 40px;
+	justify-content: center;
+	margin-top: 50px;
+	display: flex;
+	gap: 40px;
 
-    button {
-        padding: 10px 20px;
-        font-size: 1rem;
-        border-radius: 25px;
-        border: none;
-        cursor: pointer;
-        font-weight: bold;
-        width: 150px;
-    }
+	button {
+		padding: 10px 20px;
+		font-size: 1rem;
+		border-radius: 25px;
+		border: none;
+		cursor: pointer;
+		font-weight: bold;
+		width: 150px;
+	}
 
-    .signup {
-        background-color: #8a73ff;
-        color: white;
-        &:hover {
-        background-color: #7856e2;
-        }
-    }
+	.signup {
+		background-color: #8a73ff;
+		color: white;
+		&:hover {
+			background-color: #7856e2;
+		}
+	}
 
-    .call {
-        background-color: transparent;
-        border: 2px solid white;
-        color: white;
-        &:hover {
-        background-color: #069c39;
-        }
-    }
+	.call {
+		background-color: transparent;
+		border: 2px solid white;
+		color: white;
+		&:hover {
+			background-color: #069c39;
+		}
+	}
 `;
 
+
 const SignCall = () => {
-    
-    return (
-        <Container>
-            <TopLeftImage src={'/images/Logo.png'} alt="hero pic" />
-            <TopRightImage src={'/images/heroPic.png'} alt="hero pic"/>
-            <Content>
-            
-                <Header>Early Access Bonus: Join Now and Enjoy Free Lifetime Membership</Header>
-                <SubHeader>Be Among the First 10,000 to Join—Get Lifetime Access, Free Forever</SubHeader>
-                
-                <StyledRow>
+	return (
+		<Container>
+			<TopLeftImage src={'/images/Logo.png'} alt="hero pic" />
+			<TopRightImage src={'/images/heroPic.png'} alt="hero pic" />
+			<Content>
+				<SubHeader>Be Among the First 10,000 to Join—Get Lifetime Access, Free Forever</SubHeader>
+				<Header>
+					Early Access Bonus: Join Now and Enjoy <span className="FreeLife">Free Lifetime Membership</span>
+				</Header>
 
-                    <SideContainer>
-                        <SideImage src={'/images/picPic.png'} alt="picture"/>
-                        <SideImage src={'/images/HiPic.png'} alt="picture"/>
-                    </SideContainer>
+				<StyledRow>
+					<SideContainer>
+						<SideImage src={'/images/Pictureimg.png'} alt="picture" />
+						<SideImage src={'/images/hipic.png'} alt="picture" />
+					</SideContainer>
 
-                    <MiddleContainer>
-                        <MiddleImage src={'/images/headPic.png'} alt="Head Avatar"/>
-                    </MiddleContainer>
+					<MiddleContainer>
+						<MiddleImage src={'/images/headPic.png'} alt="Head Avatar" />
+					</MiddleContainer>
 
-                    <SideContainer>
-                        <SideImage src={'/images/EyePic.png'} alt="text pic" />
-                        <SideImage src={'/images/TextPic.png'} alt="text pic" />
-                    </SideContainer>
+					<SideContainer>
+						<SideImage src={'/images/EyePic.png'} alt="text pic" />
+						<SideImage src={'/images/TextPic.png'} alt="text pic" />
+					</SideContainer>
+				</StyledRow>
 
-                </StyledRow>
+				{/* <CenterImage src={'/images/headPic.png'} alt="Head Avatar" /> */}
 
-                {/* <CenterImage src={'/images/headPic.png'} alt="Head Avatar" /> */}
-
-                <ButtonSection>
-                <button className="signup">Sign Up</button>
-                <button className="call">Call me</button>
-                </ButtonSection>
-
-            </Content>
-        </Container>
-    );
+				<ButtonSection>
+					<button className="signup">Sign Up</button>
+					<button className="call">Call me</button>
+				</ButtonSection>
+			</Content>
+		</Container>
+	);
 };
 
 export default SignCall;
