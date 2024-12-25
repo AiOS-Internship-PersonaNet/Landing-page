f5e3e0fabc3204dfc07511ca4a0e554ddf4e8fc9
devparpiev <rakhmatullo@aios.agency>
2024-12-25 00:52:50 -0600

    added term and policy files and updated footer and main texts

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,17 @@
+// app/page.jsx
 "use client";
 
 import HeroSection from "./HeroSection";
-import Footer from '@/components/footer';
-import Main from '@/components/main';
-
-
+import Footer from "@/components/footer";
+import Main from "@/components/main";
 
 export default function Home() {
-  return <>
-  <HeroSection/>
-  <Main/>
-  <Footer/>
-  </>;
+  return (
+    <>
+      <HeroSection />
+      <Main />
+      <Footer />
+    </>
+  );
 }
+
