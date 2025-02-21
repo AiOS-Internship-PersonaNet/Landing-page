332895fb103515a0e898b4941c8d431e2a7ef4ac
SidharthBabu121 <sidharthbabukish121@gmail.com>
2025-02-21 12:06:46 -0800

    Finished adding stuff in Collecting and Using your personal data

--- app/privacy-policy/page.jsx
+++ app/privacy-policy/page.jsx
@@ -172,12 +172,14 @@ export default function PrivacyPolicy() {
             <AccordionItem value="collecting-data">
               <AccordionTrigger>Collecting and Using Your Personal Data</AccordionTrigger>
               <AccordionContent>
-                <div className="space-y-4">
+              <div className="space-y-4">
                   <h3 className="font-medium">Types of Data Collected</h3>
+
                   <h4 className="font-medium text-sm">Personal Data</h4>
                   <p>
                     While using Our Service, We may ask You to provide Us with certain personally identifiable
-                    information that can be used to contact or identify You. This may include:
+                    information that can be used to contact or identify You. Personally identifiable information may
+                    include, but is not limited to:
                   </p>
                   <ul className="list-disc pl-6 space-y-2">
                     <li>Email address</li>
@@ -186,13 +188,128 @@ export default function PrivacyPolicy() {
                   </ul>
 
                   <h4 className="font-medium text-sm">Usage Data</h4>
-                  <p>Usage Data is collected automatically when using the Service. This may include:</p>
+                  <p>
+                    Usage Data is collected automatically when using the Service. Usage Data may include information
+                    such as Your Device's Internet Protocol address (e.g. IP address), browser type, browser version,
+                    the pages of our Service that You visit, the time and date of Your visit, the time spent on those
+                    pages, unique device identifiers and other diagnostic data.
+                  </p>
+                  <p>
+                    When You access the Service by or through a mobile device, We may collect certain information
+                    automatically, including, but not limited to, the type of mobile device You use, Your mobile device
+                    unique ID, the IP address of Your mobile device, Your mobile operating system, the type of mobile
+                    Internet browser You use, unique device identifiers and other diagnostic data.
+                  </p>
+                  <p>
+                    We may also collect information that Your browser sends whenever You visit our Service or when You
+                    access the Service by or through a mobile device.
+                  </p>
+
+                  <h4 className="font-medium text-sm">Information from Third-Party Social Media Services</h4>
+                  <p>
+                    The Company allows You to create an account and log in to use the Service through the following
+                    Third-party Social Media Services:
+                  </p>
                   <ul className="list-disc pl-6 space-y-2">
-                    <li>Browser type and version</li>
-                    <li>Time spent on pages</li>
-                    <li>Device information</li>
-                    <li>IP address</li>
+                    <li>Google</li>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>LinkedIn</li>
                   </ul>
+                  <p>
+                    If You decide to register through or otherwise grant us access to a Third-Party Social Media
+                    Service, We may collect Personal data that is already associated with Your Third-Party Social Media
+                    Service's account, such as Your name, Your email address, Your activities or Your contact list
+                    associated with that account.
+                  </p>
+                  <p>
+                    You may also have the option of sharing additional information with the Company through Your
+                    Third-Party Social Media Service's account. If You choose to provide such information and Personal
+                    Data, during registration or otherwise, You are giving the Company permission to use, share, and
+                    store it in a manner consistent with this Privacy Policy.
+                  </p>
+
+                  <h4 className="font-medium text-sm">Tracking Technologies and Cookies</h4>
+                  <p>
+                    We use Cookies and similar tracking technologies to track the activity on Our Service and store
+                    certain information. Tracking technologies used are beacons, tags, and scripts to collect and track
+                    information and to improve and analyze Our Service. The technologies We use may include:
+                  </p>
+                  <ul className="list-disc pl-6 space-y-2">
+                    <li>
+                      <strong>Cookies or Browser Cookies:</strong> A cookie is a small file placed on Your Device. You
+                      can instruct Your browser to refuse all Cookies or to indicate when a Cookie is being sent.
+                      However, if You do not accept Cookies, You may not be able to use some parts of our Service.
+                      Unless you have adjusted Your browser setting so that it will refuse Cookies, our Service may use
+                      Cookies.
+                    </li>
+                    <li>
+                      <strong>Web Beacons:</strong> Certain sections of our Service and our emails may contain small
+                      electronic files known as web beacons (also referred to as clear gifs, pixel tags, and
+                      single-pixel gifs) that permit the Company, for example, to count users who have visited those
+                      pages or opened an email and for other related website statistics (for example, recording the
+                      popularity of a certain section and verifying system and server integrity).
+                    </li>
+                  </ul>
+                  <p>
+                    Cookies can be "Persistent" or "Session" Cookies. Persistent Cookies remain on Your personal
+                    computer or mobile device when You go offline, while Session Cookies are deleted as soon as You
+                    close Your web browser.
+                  </p>
+                  <p>We use both Session and Persistent Cookies for the purposes set out below:</p>
+                  <ul className="list-disc pl-6 space-y-2">
+                    <li>
+                      <strong>Necessary / Essential Cookies</strong>
+                      <br />
+                      Type: Session Cookies
+                      <br />
+                      Administered by: Us
+                      <br />
+                      Purpose: These Cookies are essential to provide You with services available through the Website
+                      and to enable You to use some of its features. They help to authenticate users and prevent
+                      fraudulent use of user accounts. Without these Cookies, the services that You have asked for
+                      cannot be provided, and We only use these Cookies to provide You with those services.
+                    </li>
+                    <li>
+                      <strong>Cookies Policy / Notice Acceptance Cookies</strong>
+                      <br />
+                      Type: Persistent Cookies
+                      <br />
+                      Administered by: Us
+                      <br />
+                      Purpose: These Cookies identify if users have accepted the use of cookies on the Website.
+                    </li>
+                    <li>
+                      <strong>Functionality Cookies</strong>
+                      <br />
+                      Type: Persistent Cookies
+                      <br />
+                      Administered by: Us
+                      <br />
+                      Purpose: These Cookies allow us to remember choices You make when You use the Website, such as
+                      remembering your login details or language preference. The purpose of these Cookies is to provide
+                      You with a more personal experience and to avoid You having to re-enter your preferences every
+                      time You use the Website.
+                    </li>
+                    <li>
+                      <strong>Tracking and Performance Cookies</strong>
+                      <br />
+                      Type: Persistent Cookies
+                      <br />
+                      Administered by: Third-Parties
+                      <br />
+                      Purpose: These Cookies are used to track information about traffic to the Website and how users
+                      use the Website. The information gathered via these Cookies may directly or indirectly identify
+                      you as an individual visitor. This is because the information collected is typically linked to a
+                      pseudonymous identifier associated with the device you use to access the Website. We may also use
+                      these Cookies to test new pages, features or new functionality of the Website to see how our users
+                      react to them.
+                    </li>
+                  </ul>
+                  <p>
+                    For more information about the cookies we use and your choices regarding cookies, please visit our
+                    Cookies Policy or the Cookies section of our Privacy Policy.
+                  </p>
                 </div>
               </AccordionContent>
             </AccordionItem>
