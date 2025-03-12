485a575743674e1d06bf36bc71b58e1304e2047b
Bharath <bharathkv15899@gmail.com>
2025-03-12 17:16:29 -0500

    streaming responses

--- components/ChatBox.jsx
+++ components/ChatBox.jsx
@@ -61,7 +61,6 @@ export default function ChatBox({ onClose }) {
     })();
   }, [selectedUser]);
 
-  h
   // A function streams the bot response word by word
   const streamBotResponse = (fullMessage, delay = 100) => {
     const words = fullMessage.split(" ");
